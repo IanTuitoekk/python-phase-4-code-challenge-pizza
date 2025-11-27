4cd208a64102961597013081275af91c8a47480d
Ian Tuitoek <iantuitoek4@gmail.com>
2025-11-27 20:25:56 +0300

    Refactored the code

--- server/app.py
+++ server/app.py
@@ -25,5 +25,68 @@ def index():
     return "<h1>Code challenge</h1>"
 
 
+class Restaurants(Resource):
+    def get(self):
+        restaurants = Restaurant.query.all()
+        return [restaurant.to_dict(only=('id', 'name', 'address')) for restaurant in restaurants], 200
+
+
+class RestaurantByID(Resource):
+    def get(self, id):
+        restaurant = Restaurant.query.filter_by(id=id).first()
+        
+        if not restaurant:
+            return {"error": "Restaurant not found"}, 404
+        
+        return restaurant.to_dict(only=('id', 'name', 'address', 'restaurant_pizzas.id', 'restaurant_pizzas.price', 'restaurant_pizzas.pizza_id', 'restaurant_pizzas.restaurant_id', 'restaurant_pizzas.pizza.id', 'restaurant_pizzas.pizza.name', 'restaurant_pizzas.pizza.ingredients')), 200
+    
+    def delete(self, id):
+        restaurant = Restaurant.query.filter_by(id=id).first()
+        
+        if not restaurant:
+            return {"error": "Restaurant not found"}, 404
+        
+        db.session.delete(restaurant)
+        db.session.commit()
+        
+        return '', 204
+
+
+class Pizzas(Resource):
+    def get(self):
+        pizzas = Pizza.query.all()
+        return [pizza.to_dict(only=('id', 'name', 'ingredients')) for pizza in pizzas], 200
+
+
+class RestaurantPizzas(Resource):
+    def post(self):
+        try:
+            data = request.get_json()
+            
+            # Create new RestaurantPizza
+            new_restaurant_pizza = RestaurantPizza(
+                price=data.get('price'),
+                pizza_id=data.get('pizza_id'),
+                restaurant_id=data.get('restaurant_id')
+            )
+            
+            db.session.add(new_restaurant_pizza)
+            db.session.commit()
+            
+            return new_restaurant_pizza.to_dict(only=('id', 'price', 'pizza_id', 'restaurant_id', 'pizza.id', 'pizza.name', 'pizza.ingredients', 'restaurant.id', 'restaurant.name', 'restaurant.address')), 201
+        
+        except ValueError as e:
+            return {"errors": ["validation errors"]}, 400
+        except Exception as e:
+            return {"errors": ["validation errors"]}, 400
+
+
+# Register resources with routes
+api.add_resource(Restaurants, '/restaurants')
+api.add_resource(RestaurantByID, '/restaurants/<int:id>')
+api.add_resource(Pizzas, '/pizzas')
+api.add_resource(RestaurantPizzas, '/restaurant_pizzas')
+
+
 if __name__ == "__main__":
     app.run(port=5555, debug=True)
